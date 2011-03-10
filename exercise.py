49302066c43a7973baf352c67c56f701b323d655
Robbie Clemons <RobClemons@gmail.com>
2011-03-09 20:54:57 -0500

    Now exercise quits drawing a users if their joints haven't been updated lately

--- exercise.py
+++ exercise.py
@@ -47,22 +47,21 @@ frame_count = 0
 users = {}
 
 class Player(Skeleton):
+    """Holds a players data.  Inherits from Skeleton
+    
+    Player.hits is a counter for the number of targets a player hits
+    
+    Player.old is a counter for the number of frames without a player
+    """
     
     def __init__(self, user_id):
         Skeleton.__init__(self, user_id)
         self.hits = 0
-        self.history = deque(maxlen = 10)
-        self.old_frames = 0
-    
-    def add_frame(self, skel):
-        if len(self.joints) > 0:
-            self.history.append(self.joints)
-            self.joints.clear()
-        for joint_name, pnt  in skel.joints.items():
-            self[joint_name] = pnt.copy()
-    
+        self.old = 0
+        
     def still_moving(self):
-        return self.joints != history[0]
+        """Tests whether the Player's joints have been updated recently"""
+        return self.old < len(users) * 2
         
 class Target(Point):
     """Stores target information"""
@@ -160,31 +159,32 @@ def glutIdle():
             if player.id not in users:
                 users[player.id] = Player(player.id)
             if users[player.id].joints != player.joints:
-                users[player.id].add_frame(player) 
+                users[player.id].joints = player.joints 
                 frame_count = server.frames
                 glutPostRedisplay()
-                users[player.id].old_frames = 0
+                users[player.id].old = 0
             else:
-                users[player.id].old_frames += 1
+                users[player.id].old += 1
                 
 def drawPlayers():
     """Draws lines connecting available joints for every player in users"""
     glBegin(GL_LINES)
     for player in users.values():
-        drawLine(player, HEAD, NECK)
-        drawLine(player, NECK, TORSO)
-        drawLine(player, LEFT_SHOULDER, LEFT_HIP)
-        drawLine(player, RIGHT_SHOULDER, RIGHT_HIP)
-        drawLine(player, LEFT_HIP, RIGHT_HIP)
-        drawLine(player, LEFT_HIP, LEFT_KNEE)
-        drawLine(player, LEFT_KNEE, LEFT_FOOT)
-        drawLine(player, RIGHT_HIP, RIGHT_KNEE)
-        drawLine(player, RIGHT_KNEE, RIGHT_FOOT)
-        drawLine(player, LEFT_SHOULDER, RIGHT_SHOULDER)
-        drawLine(player, LEFT_SHOULDER, LEFT_ELBOW)
-        drawLine(player, LEFT_ELBOW, LEFT_HAND)   
-        drawLine(player, RIGHT_SHOULDER, RIGHT_ELBOW)
-        drawLine(player, RIGHT_ELBOW, RIGHT_HAND)
+        if player.still_moving():
+            drawLine(player, HEAD, NECK)
+            drawLine(player, NECK, TORSO)
+            drawLine(player, LEFT_SHOULDER, LEFT_HIP)
+            drawLine(player, RIGHT_SHOULDER, RIGHT_HIP)
+            drawLine(player, LEFT_HIP, RIGHT_HIP)
+            drawLine(player, LEFT_HIP, LEFT_KNEE)
+            drawLine(player, LEFT_KNEE, LEFT_FOOT)
+            drawLine(player, RIGHT_HIP, RIGHT_KNEE)
+            drawLine(player, RIGHT_KNEE, RIGHT_FOOT)
+            drawLine(player, LEFT_SHOULDER, RIGHT_SHOULDER)
+            drawLine(player, LEFT_SHOULDER, LEFT_ELBOW)
+            drawLine(player, LEFT_ELBOW, LEFT_HAND)   
+            drawLine(player, RIGHT_SHOULDER, RIGHT_ELBOW)
+            drawLine(player, RIGHT_ELBOW, RIGHT_HAND)
     glEnd()
     
 def drawTarget():
@@ -192,52 +192,53 @@ def drawTarget():
     glMatrixMode(GL_MODELVIEW)
     glLineWidth(1)
     for player in users.values():
-        targ = users_targets[player.hits % len(users_targets)]
-        if (targ.base_joint, targ.middle_joint, targ.hit_joint) in player:
-            orientation = getPlayersOrientation(player)
-            #draws a sphere on the joint the player has to use
-            glPushMatrix()
-            ball = player[targ.hit_joint]
-            glTranslate(ball.x, ball.y, ball.z)
-            glColor3f(1, 1, 1)
-            glutSolidSphere(TARGET_SIZE/ 2.0, 30, 30)
-            glPopMatrix()
-            glPushMatrix()
-            #rotates target along the y axis, ie user turns side to side
-            yRotMat = np.array([[np.cos(orientation.x * -np.pi/2.0), 0, -np.sin(orientation.x * -np.pi/2.0)],
-                      [0, 1, 0],
-                      [np.sin(orientation.x * -np.pi/2.0), 0, np.cos(orientation.x * -np.pi/2.0)]])
-            #rotates target along the x axis, ie user leans forward or back
-            xRotMat = np.array([[1, 0, 0],
-                      [0, np.cos(orientation.y * np.pi/2.0), np.sin(orientation.y * np.pi/2.0)],
-                      [0, -np.sin(orientation.y * np.pi/2.0), np.cos(orientation.y * np.pi/2.0)]])
-            #determines target position based on limb length
-            if targ.calc_len:
-                mag = (player[targ.middle_joint] - player[targ.base_joint]).magnitude()
-                mag += (player[targ.hit_joint] - player[targ.middle_joint]).magnitude()
-                targ.normalize()
-                targList = [targ.x * mag,  targ.y * mag, targ.z * mag]
-            else:
-                targList = targ.vals()
-            targList = np.dot(yRotMat, targList)
-            targList = np.dot(xRotMat, targList)
-            targPoint = Point(targList[0], targList[1], targList[2])
-            targPoint += player[targ.base_joint]
-            glTranslate(targPoint.x, targPoint.y, targPoint.z)
-            #target is hit if hit_joint is inside target
-            ht = player[targ.hit_joint] - targPoint
-            if abs(ht.x) < TARGET_SIZE and abs(ht.y) < TARGET_SIZE and abs(ht.z) < TARGET_SIZE:
-                r, g, b = (1, 1, 1)
-                player.hits += 1
-            else:
-                r, g, b = getRGB(targPoint)
-            glRotatef(orientation.x * 90, 0, 1, 0)
-            glRotatef(orientation.y * -90, 1, 0, 0)
-            glColor3f(r, g, b)
-            glutSolidCube(TARGET_SIZE)
-            glColor3f(0, 0, 0)
-            glutWireCube(TARGET_SIZE + 1)
-            glPopMatrix()        
+        if player.still_moving():
+            targ = users_targets[player.hits % len(users_targets)]
+            if (targ.base_joint, targ.middle_joint, targ.hit_joint) in player:
+                orientation = getPlayersOrientation(player)
+                #draws a sphere on the joint the player has to use
+                glPushMatrix()
+                ball = player[targ.hit_joint]
+                glTranslate(ball.x, ball.y, ball.z)
+                glColor3f(1, 1, 1)
+                glutSolidSphere(TARGET_SIZE/ 2.0, 30, 30)
+                glPopMatrix()
+                glPushMatrix()
+                #rotates target along the y axis, ie user turns side to side
+                yRotMat = np.array([[np.cos(orientation.x * -np.pi/2.0), 0, -np.sin(orientation.x * -np.pi/2.0)],
+                          [0, 1, 0],
+                          [np.sin(orientation.x * -np.pi/2.0), 0, np.cos(orientation.x * -np.pi/2.0)]])
+                #rotates target along the x axis, ie user leans forward or back
+                xRotMat = np.array([[1, 0, 0],
+                          [0, np.cos(orientation.y * np.pi/2.0), np.sin(orientation.y * np.pi/2.0)],
+                          [0, -np.sin(orientation.y * np.pi/2.0), np.cos(orientation.y * np.pi/2.0)]])
+                #determines target position based on limb length
+                if targ.calc_len:
+                    mag = (player[targ.middle_joint] - player[targ.base_joint]).magnitude()
+                    mag += (player[targ.hit_joint] - player[targ.middle_joint]).magnitude()
+                    targ.normalize()
+                    targList = [targ.x * mag,  targ.y * mag, targ.z * mag]
+                else:
+                    targList = targ.vals()
+                targList = np.dot(yRotMat, targList)
+                targList = np.dot(xRotMat, targList)
+                targPoint = Point(targList[0], targList[1], targList[2])
+                targPoint += player[targ.base_joint]
+                glTranslate(targPoint.x, targPoint.y, targPoint.z)
+                #target is hit if hit_joint is inside target
+                ht = player[targ.hit_joint] - targPoint
+                if abs(ht.x) < TARGET_SIZE and abs(ht.y) < TARGET_SIZE and abs(ht.z) < TARGET_SIZE:
+                    r, g, b = (1, 1, 1)
+                    player.hits += 1
+                else:
+                    r, g, b = getRGB(targPoint)
+                glRotatef(orientation.x * 90, 0, 1, 0)
+                glRotatef(orientation.y * -90, 1, 0, 0)
+                glColor3f(r, g, b)
+                glutSolidCube(TARGET_SIZE)
+                glColor3f(0, 0, 0)
+                glutWireCube(TARGET_SIZE + 1)
+                glPopMatrix()        
     
 def getPlayersOrientation(player):
     """Determines a users orientation.
